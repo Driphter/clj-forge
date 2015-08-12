84a63c1e57811d1c687e10c81e8a99b0c852d49d
Driphter <TheDriphter@gmail.com>
2015-08-12 13:35:23 -0400

    Made the gradle config better. I think.

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-gradlew clean jar
-for f in build/libs/*.jar
-  do
-    name=`echo ${f} | sed -E 's/(.*)(-([0-9]\.)*[0-9])\.jar/\1-deobf\2.jar/'`
-    cp "$f" "$name"
-  done
-gradlew build
--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
             :dependencies [[org.clojure/clojure "1.6.0"]
                            [com.google.guava/guava "18.0"]
                            [org.apache.logging.log4j/log4j-core "2.3"]
-                           [net.minecraftforge/forgeSrc "1.7.10-10.13.3.1403-1.7.10"]]
+                           [net.minecraftforge/forgeSrc "1.7.10-10.13.4.1448-1.7.10"]]
             :plugins [[lein-cljfmt "0.1.10"]
                       [lein-kibit "0.1.2"]
                       [jonase/eastwood "0.2.1"]]
